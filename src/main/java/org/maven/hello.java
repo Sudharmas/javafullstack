c4c873e3f93c6c32c862bcb214166a958c431334
Sudharmas <sudharmasuvarna@gmail.com>
2024-01-08 21:14:54 +0530

    javafullstack

---
+++ src/main/java/org/maven/hello.java
@@ -0,0 +1,11 @@
+package org.maven;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        int x=5;
+        x++;
+        System.out.println((x));
+
+    }
+}
