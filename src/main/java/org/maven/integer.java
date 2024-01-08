84086e2dddc1eba9d1aa1899f328d46143aefe50
Sudharmas <sudharmasuvarna@gmail.com>
2024-01-08 21:14:54 +0530

    javafullstack

---
+++ src/main/java/org/maven/integer.java
@@ -0,0 +1,10 @@
+package org.maven;
+
+public class integer {
+    public static void main(String[] args) {
+        float a=10f;
+        long b =(int)a;
+        System.out.println(b);
+    }
+
+}
