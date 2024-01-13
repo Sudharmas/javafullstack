49d90fccbf2bacc4e096ad15fddebcb330bc1e38
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-01-13 23:45:43 +0530

    Update integer.java

--- src/main/java/org/maven/integer.java
+++ src/main/java/org/maven/integer.java
@@ -2,8 +2,8 @@ package org.maven;
 
 public class integer {
     public static void main(String[] args) {
-        float a=10f;
-        long b =(int)a;
+        float a=10f;//float type is initialized
+        long b =(int)a;//initializing b to value of a after converting a from float to int
         System.out.println(b);
     }
 
