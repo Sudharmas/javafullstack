5b0132fa4d50ecde5417c9494b602ff0701e82fb
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-01-14 23:22:00 +0530

    for_loop.java

--- src/main/java/org/maven/for_loop.java
+++ src/main/java/org/maven/for_loop.java
@@ -2,10 +2,14 @@ package org.maven;
 
 public class for_loop {
     public static void main(String[] args) {
-        int a = 10;
-        //
+        int a = 10;//initializing variable
+        //declaring the for loop
+        // for loop has 3 statements in it. Initialization,Condition and Step(Increment / Decrement)
+        //  Syntax : for(Initialization ; Condition ; Step(Increment/Decrement){
+        // statements;
+        // }
         for (int i = 0; i < a; i++) {
-            System.out.println("\n i:" + i);
+            System.out.println("\n i:" + i);//printing i values from 0 to 9
         }
     }
 }
