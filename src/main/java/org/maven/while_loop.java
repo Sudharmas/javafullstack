7ef1599110be4ff3510a4979638c5f278c1dfe41
java-begi <Sudharma1604@gmail.com>
2024-01-14 23:27:37 +0530

    Merge pull request #2 from Sudharmas/revert-1-Sudharma_s-patch-1
    Revert "if_stat.java"

---
+++ src/main/java/org/maven/while_loop.java
@@ -0,0 +1,11 @@
+package org.maven;
+
+public class while_loop {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i<=10){
+            System.out.println("i:"+i);
+            i++;
+        }
+    }
+}
