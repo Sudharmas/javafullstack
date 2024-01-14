5a0e7642fbb02888858c66fdfeadc953b4705c11
java-begi <Sudharma1604@gmail.com>
2024-01-14 23:17:10 +0530

    Merge pull request #2 from Sudharmas/revert-1-Sudharma_s-patch-1
    Revert "if_stat.java"

---
+++ src/main/java/org/maven/for_loop.java
@@ -0,0 +1,11 @@
+package org.maven;
+
+public class for_loop {
+    public static void main(String[] args) {
+        int a = 10;
+        //
+        for (int i = 0; i < a; i++) {
+            System.out.println("\n i:" + i);
+        }
+    }
+}
