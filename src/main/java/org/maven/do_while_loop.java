47de568b17e86868cdb52a62f23135ade432abd5
java-begi <Sudharma1604@gmail.com>
2024-01-15 23:50:20 +0530

    while loop

---
+++ src/main/java/org/maven/do_while_loop.java
@@ -0,0 +1,12 @@
+package org.maven;
+
+public class do_while_loop {
+    public static void main(String[] args) {
+
+        int i = 0;//initializing i = 0
+        do {
+            System.out.println("i=" + i);
+            i++;//incrementing value of i
+        } while (i < 10);
+    }
+}
