3540e143de79e92d6e4a2803819f99a781320780
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-01-15 23:52:43 +0530

    do_while_loop.java

--- src/main/java/org/maven/do_while_loop.java
+++ src/main/java/org/maven/do_while_loop.java
@@ -4,8 +4,9 @@ public class do_while_loop {
     public static void main(String[] args) {
 
         int i = 0;//initializing i = 0
+        //do while loop is executes the do{} statement atleast once and then checks the while condition
         do {
-            System.out.println("i=" + i);
+            System.out.println("i=" + i);//printing value of i
             i++;//incrementing value of i
         } while (i < 10);
     }
