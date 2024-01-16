ab7b68e6413b7c57535d27a611b715baf72e934d
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-01-16 23:52:50 +0530

    nested_for_loop.java

--- src/main/java/org/maven/nested_for_loop.java
+++ src/main/java/org/maven/nested_for_loop.java
@@ -3,7 +3,8 @@ package org.maven;
 public class nested_for_loop {
     public static void main(String[] args) {
 
-
+// we use nested for loops to initailize matrices and multidimensional arrays,stacks,linked lists,etc..
+        //for loop written inside another for loop is a nested loop.
         for (int i = 0; i < 5; i++) {
             for (int j = 0;j  < 5;j ++) {
                 System.out.println("i:"+i+" j:"+j);
