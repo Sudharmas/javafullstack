fc389b540f117c26f022e98c2c3f345cb328473b
java-begi <Sudharma1604@gmail.com>
2024-01-16 23:50:46 +0530

    while loop

--- src/main/java/org/maven/nested_for_loop.java
+++ src/main/java/org/maven/nested_for_loop.java
@@ -2,10 +2,10 @@ package org.maven;
 
 public class nested_for_loop {
     public static void main(String[] args) {
-        int i=0;
-        int j=0;
-        for (i = 0; i < 5; i++) {
-            for (j = 0;j  < 5;j ++) {
+
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0;j  < 5;j ++) {
                 System.out.println("i:"+i+" j:"+j);
             }
         }
