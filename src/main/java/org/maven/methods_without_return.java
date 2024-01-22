012dc3d3d064b7afd5a3075dc5fd5ed69717ad62
java-begi <Sudharma1604@gmail.com>
2024-01-22 22:37:58 +0530

    while loop

---
+++ src/main/java/org/maven/methods_without_return.java
@@ -0,0 +1,12 @@
+package org.maven;
+
+public class methods_without_return {
+//    int area = areaofrectangle(10,20);
+    public static void main(String[] args) {
+        area(10,20);
+
+    }
+    public static void area(int height,int breadth){
+        System.out.println("area is : "+ height*breadth);
+    }
+}
