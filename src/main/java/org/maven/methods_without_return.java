616a6dbfe2192c59aa2c507b38562441001525b8
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-01-22 22:40:19 +0530

    methods_without_return.java

--- src/main/java/org/maven/methods_without_return.java
+++ src/main/java/org/maven/methods_without_return.java
@@ -1,12 +1,12 @@
 package org.maven;
 
 public class methods_without_return {
-//    int area = areaofrectangle(10,20);
+
     public static void main(String[] args) {
-        area(10,20);
+        area(10,20);//calling the function
 
     }
     public static void area(int height,int breadth){
-        System.out.println("area is : "+ height*breadth);
+        System.out.println("area is : "+ height*breadth);//display the output directly inside the method without taking the value back from methods
     }
 }
