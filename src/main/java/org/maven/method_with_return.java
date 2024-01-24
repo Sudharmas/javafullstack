9b6fd249cf682a32cda289cf695350c254d718f3
java-begi <Sudharma1604@gmail.com>
2024-01-24 23:58:26 +0530

    while loop

---
+++ src/main/java/org/maven/method_with_return.java
@@ -0,0 +1,11 @@
+package org.maven;
+
+public class method_with_return {
+    public static void main(String[] args) {
+        int area = areaofsquare(2);
+        System.out.println(area);
+    }
+    public static int areaofsquare(int x){
+        return x*x;
+    }
+}
