9063cdcd75f0ccc63d5a11a96c06856799e230de
java-begi <Sudharma1604@gmail.com>
2024-01-25 22:38:21 +0530

    while loop

---
+++ src/main/java/org/maven/initializing_variables.java
@@ -0,0 +1,15 @@
+package org.maven;
+
+public class initializing_variables {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 10f;
+        double c = 2D;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
+//10
+//10.0
+// 2.0
