efae306f93ae8c3c3d714c7e106bdab258a71caa
java-begi <Sudharma1604@gmail.com>
2024-01-30 23:07:43 +0530

    while loop

---
+++ src/main/java/org/maven/switch_case.java
@@ -0,0 +1,15 @@
+package org.maven;
+
+public class switch_case {
+    public static void main(String[] args) {
+        int n = 1;
+        switch (n){
+            case 1 :
+                System.out.println(n);
+                break;
+                case 2 :
+                System.out.println(n+1);
+                break;
+        }
+    }
+}
