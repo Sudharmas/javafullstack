2bb8f75ee41363d2bb2de2eb0cb65d62e7321c37
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-01-31 22:39:21 +0530

    classandobjects.java

--- src/main/java/classes/classandobjects.java
+++ src/main/java/classes/classandobjects.java
@@ -1,6 +1,7 @@
 package classes;
 
-public class classandobjects {
+public class classandobjects { // creating a class named classandobjects
+    // private is a access specifier used to declare variables so that those variables cannot be accessed or used  
     private String color;
     private int number;
     private String name;
