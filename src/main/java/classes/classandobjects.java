fd524a8dd9e8f2cb9f33c8c6eef47e5beba7db67
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-01-31 22:41:42 +0530

    classandobjects.java

--- src/main/java/classes/classandobjects.java
+++ src/main/java/classes/classandobjects.java
@@ -1,9 +1,11 @@
 package classes;
 
 public class classandobjects { // creating a class named classandobjects
-    // private is a access specifier used to declare variables so that those variables cannot be accessed or used  
+    // private is a access specifier,it's used to declare variables and methods so that they cannot be accessed or used outside the class that they have been declared 
+    // they can only be used inside the class where they have been declared
     private String color;
     private int number;
     private String name;
+    // public is a access specifier , variables declared as public can be used in any class using objects.
     public int speed=100;
 }
