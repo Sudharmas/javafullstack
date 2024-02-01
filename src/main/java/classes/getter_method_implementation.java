237abc6eb86a9ee17cce2ad6a2f77a1b74ccfe83
java-begi <Sudharma1604@gmail.com>
2024-02-01 20:36:19 +0530

    getter methods

---
+++ src/main/java/classes/getter_method_implementation.java
@@ -0,0 +1,9 @@
+package classes;
+
+public class getter_method_implementation {
+    public static void main(String[] args) {
+        getter_methods obj = new getter_methods();
+        System.out.println(obj.getx());
+    }
+
+}
---
+++ src/main/java/classes/getter_methods.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class getter_methods {
+    private int x = 5;
+    private  int y;
+    private int z;
+
+    public int getx(){
+        return x;
+    }
+}
