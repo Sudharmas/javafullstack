19210d6537343ccf5b6cbfadf34d88154e43eb09
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-01 20:43:08 +0530

    getter_methods.java

--- src/main/java/classes/getter_methods.java
+++ src/main/java/classes/getter_methods.java
@@ -1,11 +1,20 @@
 package classes;
 
 public class getter_methods {
+    // here the variables are fdecalred as private ,and they are only accessible inside this class
     private int x = 5;
     private  int y;
     private int z;
-
+// but if we want to use these variables in another class or display it in another class then we use getter methods
+    // getter methods are named with 'get'prefix added to the varaibles that will be used
     public int getx(){
-        return x;
+        return x; // here we are returning the variables to the method which calls this getx method. 
+        // we can only use or return a variable if we use getter methods,else we cannot access the variables in another function
     }
 }
+
+/*
+out put
+    5
+    */
+// NOTE: The output will be zero if we donot initialize the variable and if we access the variable 
