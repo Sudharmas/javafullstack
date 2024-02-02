8af9d16fccbd102c2b5c1e3e92422600edbea4cf
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-02 23:54:31 +0530

    getter_method_implementation.java

--- src/main/java/classes/getter_method_implementation.java
+++ src/main/java/classes/getter_method_implementation.java
@@ -1,9 +1,16 @@
 package classes;
 
 public class getter_method_implementation {
+     // we can get the variable x declared in the class getter_methods using objects and calling getx method
     public static void main(String[] args) {
-        getter_methods obj = new getter_methods();
+        getter_methods obj = new getter_methods();//object creation
+                // obj.getx() calls getx method which returns value of x to this function
         System.out.println(obj.getx());
     }
 
 }
+
+/*
+output
+10 
+*/
