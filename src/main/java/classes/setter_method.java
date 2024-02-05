ebf3f60c641c3e30d23f08207c1970442cb96934
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-05 23:24:17 +0530

    setter_method.java

--- src/main/java/classes/setter_method.java
+++ src/main/java/classes/setter_method.java
@@ -3,12 +3,13 @@ package classes;
 public class setter_method {
     private int x=5;
 
+    // setter methods are used to set the values for private variavbles only if needed,else ther value cannot be changed.
+    // syntax: public void set<var>(parameter){ this.var = var; }
+    
     public void setX(int x) {
-        this.x = x;
+        this.x = x;//this keyword is used to assign value to variable,we cannot assign the value to variable ,so we use this keyword to assign new value to variable.
 
     }
 
-    public int getX() {
-        return x;
-    }
+
 }
