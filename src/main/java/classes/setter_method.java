e5869be6f57fd62af48011dd51784e05c837fb8b
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-05 23:18:46 +0530

    Add files via upload

---
+++ src/main/java/classes/setter_method.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class setter_method {
+    private int x=5;
+
+    public void setX(int x) {
+        this.x = x;
+
+    }
+
+    public int getX() {
+        return x;
+    }
+}
