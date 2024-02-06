86adf90291a7afa3ad3c1e5dbcdb8bc03910acdb
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-06 23:05:00 +0530

    Add files via upload

---
+++ src/main/java/classes/setter_implementation.java
@@ -0,0 +1,10 @@
+package classes;
+
+public class setter_implementation {
+    public static void main(String[] args) {
+        setter_method obj = new setter_method();
+        obj.setX(10);
+        int a = obj.getX();
+        System.out.println(a);
+    }
+}
