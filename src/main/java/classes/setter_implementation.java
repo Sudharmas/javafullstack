87e41c748865ca028d18fcca386babc6e248b68c
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-06 23:06:11 +0530

    setter_implementation.java

--- src/main/java/classes/setter_implementation.java
+++ src/main/java/classes/setter_implementation.java
@@ -3,8 +3,16 @@ package classes;
 public class setter_implementation {
     public static void main(String[] args) {
         setter_method obj = new setter_method();
+        // initially the value of x was 5 ,now we are assigning value of x to 10 by setter method.
+        // we are creating object for setter_methods class and passing parameter to setX method using object.
         obj.setX(10);
-        int a = obj.getX();
+        int a = obj.getX();//calling getX method to know that the value has been changed
         System.out.println(a);
     }
 }
+
+/*
+output:
+10
+*/
+// now the value of x changed to 10 and will be displayed
