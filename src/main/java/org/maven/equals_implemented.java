ef764e5e44dea3dc9f0a666cc35c928a2fd06270
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-07 22:48:49 +0530

    equals_implemented.java

--- src/main/java/org/maven/equals_implemented.java
+++ src/main/java/org/maven/equals_implemented.java
@@ -6,12 +6,12 @@ public class equals_implemented {
     private  String a= "Hello";
 
     public String ab(){
-        if(a.equals("Hello")){
-//            System.out.println("Equal");
-        return "Hello";
+        // .equals used to check similarity between values,i.e,if we want to chech whether the value of varaible is equal to any desired value or not.
+        if(a.equals("Hello")){// .equals check whether the string inside brackets are equal or not and then return true or false
+        return "Hello";//if equals is true then this will be returned
         }
         else {
-            return "Unequal";
+            return "Unequal";//else this will be returned
         }
     }
 }
