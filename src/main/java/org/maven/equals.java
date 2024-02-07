496bfddf84b11d4e7cee59826f96a3deb9005546
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-07 22:45:31 +0530

    Add files via upload

---
+++ src/main/java/org/maven/equals.java
@@ -0,0 +1,10 @@
+package org.maven;
+
+public class equals {
+
+    public static void main(String[] args) {
+        equals_implemented obj = new equals_implemented();
+        obj.ab();
+    }
+
+}
---
+++ src/main/java/org/maven/equals_implemented.java
@@ -0,0 +1,17 @@
+package org.maven;
+
+import java.util.Objects;
+
+public class equals_implemented {
+    private  String a= "Hello";
+
+    public String ab(){
+        if(a.equals("Hello")){
+//            System.out.println("Equal");
+        return "Hello";
+        }
+        else {
+            return "Unequal";
+        }
+    }
+}
