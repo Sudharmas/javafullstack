ae90896a67b9f2e35329c7e5b782450cb4c8692a
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-11 23:27:35 +0530

    Add files via upload

---
+++ src/main/java/classes/constructor_implementation.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class constructor_implementation {
+    public static void main(String[] args) {
+      constructor obj = new constructor(2,3,4);
+        System.out.println(obj.a);
+        System.out.println(obj.b);
+        System.out.println(obj.c);
+        
+    }
+}
