3502bc6ff5a388e3429e10dc6e8c918a20d3bf6c
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-11 23:27:12 +0530

    Add files via upload

---
+++ src/main/java/classes/constructor.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class constructor {
+    public int a;
+    public int b;
+    public int c;
+
+    public constructor(int p, int q, int r) {
+        a = p;
+        b = q;
+        c = r;
+    }
+}
