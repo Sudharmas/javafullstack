b23ab5056ac2e6181ac799a6967defca64687624
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-12 21:31:41 +0530

    constructor_implementation.java

--- src/main/java/classes/constructor_implementation.java
+++ src/main/java/classes/constructor_implementation.java
@@ -2,7 +2,9 @@ package classes;
 
 public class constructor_implementation {
     public static void main(String[] args) {
+         // creating an object for class named constructor and passing arguments for values a,b,c.
       constructor obj = new constructor(2,3,4);
+         // printing values of a,b,c
         System.out.println(obj.a);
         System.out.println(obj.b);
         System.out.println(obj.c);
