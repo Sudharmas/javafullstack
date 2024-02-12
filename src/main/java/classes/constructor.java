058d433900699015460f667a5a73a804307190ff
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-12 21:30:55 +0530

    constructor.java

--- src/main/java/classes/constructor.java
+++ src/main/java/classes/constructor.java
@@ -5,6 +5,10 @@ public class constructor {
     public int b;
     public int c;
 
+      // this is a constructor with arguments p,q,r
+    // we can manually create a constructor with same name as classname ,and always it should be as same as classname
+    // we use constructors to assign value to variables declared inside a class using objects for that class inside main function.
+    // here values are assigned to a,b,c by taking parameters from main function and storing it inside p,q,r
     public constructor(int p, int q, int r) {
         a = p;
         b = q;
