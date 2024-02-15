db81f30cb1202582192786631ff4c950c36ae391
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-15 22:06:46 +0530

    Add files via upload

---
+++ src/main/java/classes/inheritance_01.java
@@ -0,0 +1,10 @@
+package classes;
+
+public class inheritance_01 {
+    public static void main(String[] args) {
+        inheritance01 obj = new inheritance01();
+        obj.handle = "short";
+        System.out.println(obj.handle);
+
+    }
+}
