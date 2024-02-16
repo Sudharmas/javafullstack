a1832cbfe1dad07d60dbe9948859b6824e7cd2a9
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-16 23:03:07 +0530

    inheritance_01.java

--- src/main/java/classes/inheritance_01.java
+++ src/main/java/classes/inheritance_01.java
@@ -3,7 +3,8 @@ package classes;
 public class inheritance_01 {
     public static void main(String[] args) {
         inheritance01 obj = new inheritance01();
-        obj.handle = "short";
+        // creating objects to acces values of inherited class
+        obj.handle = "short";//assigning value to variable
         System.out.println(obj.handle);
 
     }
