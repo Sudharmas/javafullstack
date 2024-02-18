17399a43b410c01e0a538bbbfbae3564426c35e0
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-02-18 22:26:19 +0530

    hello.java

--- src/main/java/org/maven/hello.java
+++ src/main/java/org/maven/hello.java
@@ -2,9 +2,9 @@ package org.maven;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        int x=5;
-        x++;
+        System.out.println("Hello world");//print statement in java
+        int x=5;//initianlizing variables
+        x++;//increment , x=x+1;
         System.out.println((x));
 
     }
