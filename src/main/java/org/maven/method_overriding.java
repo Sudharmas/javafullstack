ef9f76c515bf6b93a9b4e333da332e9ea4a65f4e
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-03-12 23:43:58 +0530

    method_overriding.java

--- src/main/java/org/maven/method_overriding.java
+++ src/main/java/org/maven/method_overriding.java
@@ -12,12 +12,12 @@ public class method_overriding extends override{
     }
 }
 class override{
-    public static void area(int b) {
+    public static void area(int b) { //overriding the area
         System.out.println("area is also equal to :" + b * b *b);
     }
 }
 class override1 extends override{
-    public static void area(int c){
+    public static void area(int c){ //overriding the area again
         System.out.println("Area is also even :"+ c*c*c*c);
     }
 }
