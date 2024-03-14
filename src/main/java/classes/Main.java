3cd6d78c9b2829578e51a5854ec971bf398f406d
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-03-14 22:15:32 +0530

    Main.java

--- src/main/java/classes/Main.java
+++ src/main/java/classes/Main.java
@@ -1,7 +1,7 @@
 import org.studyeasy.Iphone;
 import org.studyeasy.Nokia3310;
 import org.studyeasy.Phone;
-
+// polymorphism can be used by crating objects for a main class and subclasses which inerits properties of main class.
 public class Main {
     public static void main(String[] args) {
 //        Phone phone = new Phone();
@@ -12,15 +12,15 @@ public class Main {
 //
 //        Iphone iphone = new Iphone();
 //        iphone.feature();
+// rather than creating every single seperate form,create a single object and then assign it various types or forms of objects.
 
-
-          Phone phone;
-
-          phone = new Phone();
+          Phone phone;//create an object for class Phone 
+// phone is an object of 'Phone' which can instantiate other forms or files using which we can easily acces them.
+          phone = new Phone();//assign various forms for object Phone and access its contents
           phone.feature();
-          phone = new Iphone();
+          phone = new Iphone();//assign various forms for object Phone and access its contents
           phone.feature();
-          phone = new Nokia3310();
+          phone = new Nokia3310();//assign various forms for object Phone and access its contents
           phone.feature();
 
     }
