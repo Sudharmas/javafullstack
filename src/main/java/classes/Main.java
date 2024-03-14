6f782df48483d4e84e55d6c64e3a4fd4d2e0268f
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-03-14 22:10:32 +0530

    Add files via upload

---
+++ src/main/java/classes/Main.java
@@ -0,0 +1,27 @@
+import org.studyeasy.Iphone;
+import org.studyeasy.Nokia3310;
+import org.studyeasy.Phone;
+
+public class Main {
+    public static void main(String[] args) {
+//        Phone phone = new Phone();
+//        phone.feature();
+//
+//        Nokia3310 nokia = new Nokia3310();
+//        nokia.feature();
+//
+//        Iphone iphone = new Iphone();
+//        iphone.feature();
+
+
+          Phone phone;
+
+          phone = new Phone();
+          phone.feature();
+          phone = new Iphone();
+          phone.feature();
+          phone = new Nokia3310();
+          phone.feature();
+
+    }
+}
