1d5175c2cec2ee180eb7d0e33b47b98caee97f16
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-03-16 23:15:46 +0530

    setter_method.java

--- src/main/java/classes/setter_method.java
+++ src/main/java/classes/setter_method.java
@@ -1,3 +1,4 @@
+
 package classes;
 
 public class setter_method {
@@ -5,11 +6,15 @@ public class setter_method {
 // NOTE: we can use stter methods to implement encapsulation using java.
     // setter methods are used to set the values for private variavbles only if needed,else ther value cannot be changed.
     // syntax: public void set<var>(parameter){ this.var = var; }
-    
+   
     public void setX(int x) {
         this.x = x;//this keyword is used to assign value to variable,we cannot assign the value to variable ,so we use this keyword to assign new value to variable.
 
     }
 
 
+}
+    public int getX() {// getter method to get or return value of x
+        return x;
+    }
 }
