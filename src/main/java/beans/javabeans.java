9f4910f5cadb6da8562c1d9c9437c7f679201b43
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-03-23 23:57:04 +0530

    Add files via upload

---
+++ src/main/java/beans/javabeans.java
@@ -0,0 +1,27 @@
+package beans;
+
+public class javabeans {
+
+	private String fname;
+	private String l_name;
+
+	public javabeans() {
+		fname = fname;
+		l_name = l_name;
+	}
+	public String getfname() {
+		return fname;
+	}
+
+	public void setfname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getL_name() {
+		return l_name;
+	}
+
+	public void setL_name(String l_name) {
+		this.l_name = l_name;
+	}
+}
