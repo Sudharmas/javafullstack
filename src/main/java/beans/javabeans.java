2d3715547d0b9285ab228a54b899b333bd80ee1d
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-04-05 21:34:37 +0530

    javabeans.java

--- src/main/java/beans/javabeans.java
+++ src/main/java/beans/javabeans.java
@@ -9,19 +9,19 @@ public class javabeans {
 		fname = fname;
 		l_name = l_name;
 	}
-	public String getfname() {
+	public String getfname() {//constructor to get the name
 		return fname;
 	}
 
-	public void setfname(String fname) {
+	public void setfname(String fname) {//constructor to update name
 		this.fname = fname;
 	}
 
-	public String getL_name() {
+	public String getL_name() {//for last name
 		return l_name;
 	}
 
-	public void setL_name(String l_name) {
+	public void setL_name(String l_name) {//for last name
 		this.l_name = l_name;
 	}
 }
