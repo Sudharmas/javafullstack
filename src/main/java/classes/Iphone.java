d3e8a7dfbfa1973b86cc5d908aced5521c967446
Sudharma <109778294+Sudharmas@users.noreply.github.com>
2024-04-20 23:50:23 +0530

    Iphone.java

--- src/main/java/classes/Iphone.java
+++ src/main/java/classes/Iphone.java
@@ -1,6 +1,6 @@
 package org.studyeasy;
 
-public class Iphone extends Phone{
+public class Iphone extends Phone{//Iphone implementing Phone
     public void feature(){
         System.out.println("make calls and do smart things");
     }
